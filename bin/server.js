76d05715ee9f09212985449c2e8f38868ce2bdd2
Moritz Onken <moritz.onken@merckgroup.com>
2019-09-24 08:06:18 -0400

    support cra

--- bin/server.js
+++ bin/server.js
@@ -1,35 +1,42 @@
 #!/usr/bin/env node
 
-const path = require('path'),
-  app = require('../index'),
-  config = require('../lib/config'),
-  args = require('yargs').env('GENESIS').options({
-    verbose: {
-      alias: 'v',
-      default: false,
-    },
-    config: {
-      alias: 'c',
-      default: 'genesis.config.yml',
-      description: 'location and name of genesis config file',
-    },
-    'webpack-config': {
-      default: 'webpack.config.js',
-      description: 'location and name of webpack config file',
-    },
-    webpack: {
-      alias: 'w',
-      default: false,
-      description: 'run webpack dev server',
-    },
-    'lambda-base-directory': {
-      alias: 'l',
-      default: path.resolve(process.cwd(), 'lambda'),
-      description: 'location of base directory that contains lambda functions',
-    }
-  }).argv;
+const path = require('path');
+const yargs = require('yargs');
+const app = require('../index');
+const config = require('../lib/config');
 
-config.load(path.resolve(args.lambdaBaseDirectory, 'api.openapi.yml')).then(res => {
-  let server = app(args, res);
-  server.listen(8080, () => console.log('Project is running at http://localhost:8080/'))
-});
+const args = yargs.env('GENESIS').options({
+  verbose: {
+    alias: 'v',
+    default: false,
+  },
+  config: {
+    alias: 'c',
+    default: 'genesis.config.yml',
+    description: 'location and name of genesis config file',
+  },
+  webserver: {
+    alias: 'w',
+    description: 'run front-end dev server, supports cra and webpack',
+  },
+  'lambda-base-directory': {
+    alias: 'l',
+    default: path.resolve(process.cwd(), 'lambda'),
+    description: 'location of base directory that contains lambda functions',
+  },
+  host: {
+    alias: 'h',
+    default: process.env.HOST || '127.0.0.1',
+  },
+  port: {
+    alias: 'p',
+    default: '3000',
+  },
+}).argv;
+
+config
+  .load(path.resolve(args.lambdaBaseDirectory, 'api.openapi.yml'))
+  .then(res => app(args, res))
+  .then((server) => {
+    server.listen(args.port, args.host, () => console.log(`Project is running at http://${args.host}:${args.port}/`));
+  });
